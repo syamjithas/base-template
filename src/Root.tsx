e4d8b3106333bacd2dce7394506c462bb397d6c8
Syamjith Sasidharan <syamjithas@gmail.com>
2022-07-04 20:50:17 +0530

    base commit

---
+++ src/Root.tsx
@@ -0,0 +1,17 @@
+import React, { Suspense } from "react";
+
+const App = React.lazy(() => import("./App"));
+
+var nativeElement: any = document.getElementById('init-loader');
+nativeElement.remove();
+
+function Root() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+    <App />
+  </Suspense>
+  );
+}
+
+
+export default Root;
