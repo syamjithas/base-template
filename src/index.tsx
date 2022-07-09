86cec6c7e3018bf869da90cf9b7e8b2fca8e41a3
Syamjith Sasidharan <syamjithas@gmail.com>
2022-07-09 15:10:53 +0530

    commit hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Root from './Root';
-import { serviceWorkerRegistration } from './utils/';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Root from './Root'
+import { serviceWorkerRegistration } from './utils/'
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
 root.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>
-);
+)
 
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.register()
