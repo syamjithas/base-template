46b09ad15d76c356cc76f413fc5960dfc2249778
Syamjith Sasidharan <syamjithas@gmail.com>
2022-07-09 19:20:35 +0530

    lint

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Root from './Root'
 import { serviceWorkerRegistration } from './utils/'
-
-if(10==5){
-  console.log("3")
-}
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
