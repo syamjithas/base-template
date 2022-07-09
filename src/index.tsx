60b844f96954d0e08becaab134a9d05916c0725a
Syamjith Sasidharan <syamjithas@gmail.com>
2022-07-09 15:11:59 +0530

    commi test

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import Root from './Root'
 import { serviceWorkerRegistration } from './utils/'
 
+if(10==5){
+  console.log("3")
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
