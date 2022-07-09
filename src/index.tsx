e1581feb5447a0dd42f252b62ae4ceef7357dc77
Syamjith Sasidharan <syamjithas@gmail.com>
2022-07-09 20:54:15 +0530

    added husky and lint-stagged

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
 
-
 root.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>
 )
+
 
 serviceWorkerRegistration.register()
