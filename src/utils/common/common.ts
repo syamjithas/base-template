6f71670b7544bc110cc57875a04068c5f5dd0e0e
Syamjith Sasidharan <syamjithas@gmail.com>
2022-07-10 12:47:26 +0530

    list fix

--- src/utils/common/common.ts
+++ src/utils/common/common.ts
@@ -1,4 +1,5 @@
 export function removeElementFromDOM () {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const nativeElement: any = document.getElementById('init-loader')
   nativeElement.remove()
 }
